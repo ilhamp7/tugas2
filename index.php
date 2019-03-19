01f8893ac524e1a2312a14a4240f1967c3b33b3e
ilhamp7 <48472174+ilhamp7@users.noreply.github.com>
2019-03-19 22:15:35 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,100 @@
+<?php
+    include 'koneksi.php';
+    
+    $no = 1;
+    $data = mysqli_query($kon,"select * from mahasiswa");
+?>
 
-<h1>Hello World</h1> <br/><table>
-    <tr>
-        <td>NIM</td>
-        <td> : </td>
-        <td>16.01.53.0143</td>
-    </tr>
-    <tr>
-        <td>Nama</td>
-        <td> : </td>
-        <td>Sri Tunjung Sujatmiko</td>
-    </tr>
-</table>
-<a href="https://unisbank.ac.id">Go To Akademik</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Akademik</title>
+    <style>
+        body {
+            font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        }
+        #customers {
+          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+          border-collapse: collapse;
+          width: 50%;
+          margin-left: 25%;
+        }
+        #customers td, #customers th {
+          border: 1px solid #ddd;
+          padding: 8px;
+        }
+        #customers tr:nth-child(even){background-color: #FFFF00;}
+        #customers tr:hover {background-color: 	#FFFFFF;}
+        #customers th {
+          padding-top: 12px;
+          padding-bottom: 12px;
+          text-align: left;
+          background-color: #dc143c;
+          color: white;
+        }
+        .tbedit {
+            background-color: #5ec3cc; /* Blue */
+            border: none;
+            color: white;
+            padding: 5px 15px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 13px;
+            margin: 4px 2px;
+            cursor: pointer;
+            
+        }
+        .tbhapus {
+            background-color: #FFFF00; /* Red */
+            border: none;
+            color: white;
+            padding: 5px 15px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 13px;
+            margin: 4px 2px;
+            cursor: pointer;
+            
+        }
+        .tbadd {
+            background-color: #FFFF00; /* Red */
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 15px;
+            margin: 4px 2px;
+            cursor: pointer;
+            
+        }
+    </style>
+</head>
+<body>
+	<p>NIM  : 16.01.53.0158</p>
+    <p>Nama : Ilham Pratama</p>
+    
+    <hr>
+    <center><h2>Data Mahasiswa</h2></center>
+    <table id="customers" border="1">
+        <tr>
+            <th>No</th>
+            <th>Nim</th>
+            <th>Nama</th>
+        </tr>
+        <?php while($d = mysqli_fetch_array($data)){?>
+        <tr>
+            <td><?=$no++?></td>
+            <td><?=$d['nim']?></td>
+            <td><?=$d['nama']?></td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id8404115_tugas2","tugas2","id8404115_akademik");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
